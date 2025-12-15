117da82c1f1c4fc15fd00723a2fd2a60d32c9e83
Hype G12 <Hype G12@RaniaRahmania>
2025-12-15 07:54:43 +0700

    initial commit

---
+++ src/main/java/com/mycompany/celciustofahrenheit1/CelciusToFahrenheit1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.celciustofahrenheit1;
+
+/**
+ *
+ * @author Hype G12
+ */
+public class CelciusToFahrenheit1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        frameUtama fu = new frameUtama();
+        fu.setLocationRelativeTo(null);
+        fu.setVisible(true);
+        
+    }
+}
