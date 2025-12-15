7a4cecf4b137a740bd9d57212fd39460be6f624d
Hype AMD <Hype AMD@HYPE>
2025-12-15 07:55:09 +0700

    initial commit

---
+++ src/hore2/Hore2.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hore2;
+
+/**
+ *
+ * @author Hype AMD
+ */
+public class Hore2 {
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
+        LoginView fu = new LoginView();
+        fu.setLocationRelativeTo(null);
+        fu.setVisible(true);
+        
+        
+    }}
+
+   
